12550fed2956911730b4fd6efee495d34e0f8c53
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2024-04-17 00:17:16 +0200

    Ejercicio 3.8

---
+++ Clase2024-2/src/tema3/ejercicios/MainAgentes.java
@@ -0,0 +1,27 @@
+package tema3.ejercicios;
+
+/** Clase de prueba del ejercicio 3.8 de agentes comerciales
+ */
+public class MainAgentes {
+	public static void main(String[] args) {
+		// TODO Descomenta estas líneas cuando tengas preparado el gestor
+//		GestorAgentes gestor = new GestorAgentes();
+//		gestor.anyadirAgente( "48#Bilbao#Luis#11111111A" );
+//		gestor.anyadirAgente( "48#Bilbao#Oihane#11111112B" );
+//		gestor.anyadirAgente( "48#Balmaseda#Mikel#11111113C" );
+//		gestor.anyadirAgente( "48#Portugalete#Ainhoa#11111114D" );
+//		gestor.anyadirAgente( "48#Las Arenas#Iker#11111115E" );
+//		gestor.anyadirAgente( "1#Gasteiz#Marta#11111116F" );
+//		gestor.anyadirAgente( "1#Gasteiz#Inma#11111117G" );
+//		gestor.anyadirAgente( "1#Gasteiz#Iñigo#11111118H" );
+//		System.out.println( "Provincias: " + gestor.getProvincias() );
+//		System.out.println( "Ciudades de 1: " + gestor.getCiudades(1) );
+//		System.out.println( "Ciudades de 48: " + gestor.getCiudades(48) );
+//		System.out.println( "Ciudades de 28: " + gestor.getCiudades(28) );
+//		System.out.println( "Agentes de Bilbao: " + gestor.getAgentes(48, "Bilbao") );
+//		System.out.println( "Agentes de Barakaldo: " + gestor.getAgentes(48, "Barakaldo") );
+//		System.out.println( "Agentes de Madrid: " + gestor.getAgentes(28, "Madrid") );
+//		System.out.println( "Agentes totales:" );
+//		gestor.informeAgentes();
+	}
+}
